2c9224b3429140863e5940f6baae3e430a0ccffd
Jacob Biros <jake@Jacobs-MacBook-Pro.local>
2017-02-12 14:07:10 +0900

    added routing feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,89 +5,53 @@ $(document).ready(function(){
     showSearchResults();
   });
 
-  appendRecommendedPlaces();
+  searchCityCode();
 
   function clearRecommendations() {
     $("#recommended-places").empty();
   }
 
   function showSearchResults() {
-    $(".bus-stop-list").append(`<a class="bus-stop" href="./bus-stop.html">
-        <span class="date">17:32発</span>
-        <span class="time">60分</span>
-        <span class="pay">合計1,180円</span>
-        <span class="name">貨物ターミナル前〔那覇空港〕</span>
-        <span class="line">沖縄バス [120]名護西空港線 名護バスターミナル行</span>
-    </a>
-    <a class="bus-stop" href="./bus-stop.html">
-        <span class="date">17:32発</span>
-        <span class="time">60分</span>
-        <span class="pay">合計1,180円</span>
-        <span class="name">貨物ターミナル前〔那覇空港〕</span>
-        <span class="line">沖縄バス [120]名護西空港線 名護バスターミナル行</span>
-    </a>
-    <a class="bus-stop" href="./bus-stop.html">
-        <span class="date">17:32発</span>
-        <span class="time">60分</span>
-        <span class="pay">合計1,180円</span>
-        <span class="name">貨物ターミナル前〔那覇空港〕</span>
-        <span class="line">沖縄バス [120]名護西空港線 名護バスターミナル行</span>
-    </a>`);
+    var date, time, pay, name, line;
+
+    var busInfoArray = [
+    { departureTime: "17:32",
+      necessaryTime: "60min",
+      sumOfPay: "¥1,180",
+      objective: "貨物ターミナル前（那覇空港）",
+      recommendationLine: "沖縄バス [120]名護西空港線 名護バスターミナル行"
+    },
+    { departureTime: "18:45",
+      necessaryTime: "25min",
+      sumOfPay: "¥1,180",
+      objective: "貨物ターミナル前（那覇空港）",
+      recommendationLine: "沖縄バス [120]名護西空港線 名護バスターミナル行"
+    },
+    { departureTime: "17:32",
+      necessaryTime: "60min",
+      sumOfPay: "¥1,180",
+      objective: "貨物ターミナル前（那覇空港）",
+      recommendationLine: "沖縄バス [120]名護西空港線 名護バスターミナル行"
+    }
+
+  ]
+  for(var index=0; index<busInfoArray.length;index++) {
+     $(".bus-stop-list").append(`<a class="bus-stop" href="./stopinfo/`+ (index + 1) +`">
+         <span class="date">` + busInfoArray[index].departureTime + `</span>
+         <span class="time">` + busInfoArray[index].necessaryTime + `</span>
+         <span class="pay">` + busInfoArray[index].sumOfPay + `</span>
+         <span class="name">` + busInfoArray[index].objective + `</span>
+         <span class="line">` + busInfoArray[index].recommendationLine + `</span>
+     </a>`);
+   }
+
   }
 
 
-  function appendRecommendedPlaces() {
-    var recommendedPlaces = [
-      { name: "沖縄美ら海水族館",
-        place: "沖縄県国頭郡本部町",
-        img: "./assets/images/spot/01.jpg",
-      },
-      { name: "海中道路",
-        place: "沖縄県うるま市",
-        img: "./assets/images/spot/02.jpg",
-      },
-      { name: "ガンガラーの谷",
-        place: "沖縄県南城市",
-        img: "./assets/images/spot/03.jpg",
-      },
-      { name: "古宇利島",
-        place: "沖縄県国頭郡今帰仁村",
-        img: "./assets/images/spot/04.jpg",
-      },
-      { name: "国際通り",
-        place: "沖縄県那覇市",
-        img: "./assets/images/spot/05.jpg",
-      },
-      { name: "琉球村",
-        place: "沖縄県国頭郡恩納村",
-        img: "./assets/images/spot/06.jpg",
-      },
-      { name: "オリオンハッピーパーク",
-        place: "沖縄県名護市",
-        img: "./assets/images/spot/07.jpg",
-      },
-      { name: "首里城公園",
-        place: "沖縄県那覇市",
-        img: "./assets/images/spot/08.jpg",
-      },
-      { name: "伊計ビーチ",
-        place: "沖縄県うるま市",
-        img: "./assets/images/spot/09.jpg",
-      },
-      { name: "もとぶ元気村",
-        place: "沖縄県国頭郡本部町",
-        img: "./assets/images/spot/10.jpg",
-      },
-      { name: "やちむんの里",
-        place: "沖縄県中頭郡読谷村",
-        img: "./assets/images/spot/11.jpg",
-      },
-      { name: "真栄田岬",
-        place: "沖縄県国頭郡恩納村",
-        img: "./assets/images/spot/12.jpg",
-      }
-    ];
 
+
+  function appendRecommendedPlaces(localSpotsList) {
+    var recommendedPlaces = localSpotsList;
     for (var index = 0; index < recommendedPlaces.length; index++){
       $("#recommended-places").append(`<a class="spot" href="./index.html">
           <img src="` + recommendedPlaces[index].img + `" alt="">
@@ -98,7 +62,70 @@ $(document).ready(function(){
       </a>`);
     }
 
-
   }
+
+  function searchCityCode() {
+    var city = "那覇市"
+    var cityList;
+    var cityCode
+    $.ajax({
+        type: "GET",
+        beforeSend: function(request) {
+          request.setRequestHeader("X-API-KEY", "A7unKhX9cxZIQJaJlQdRRhWgYbP0K8xmy1ncdX74");
+        },
+        url: "https://opendata.resas-portal.go.jp/api/v1/cities?prefCode=47",
+        success: function(msg) {
+          cityList = msg.result;
+          cityCode = $.grep(cityList, function(e){ return e.cityName == city; })[0].cityCode;
+          searchLocalSpots(cityCode)
+        }
+      });
+  }
+
+  function searchLocalSpots(cityCode) {
+    var localSpotsList;
+    $.ajax({
+        type: "GET",
+        beforeSend: function(request) {
+          request.setRequestHeader("X-API-KEY", "A7unKhX9cxZIQJaJlQdRRhWgYbP0K8xmy1ncdX74");
+        },
+        url: "https://opendata.resas-portal.go.jp/api/v1/partner/nightley/places?targetType=2&cityCode=" + cityCode + "&year=2016&seasonCode=1&periodOfTime=1",
+        success: function(result) {
+          localSpotsList = createLocalSpotObjects(result.result.cities);
+          console.log(localSpotsList);
+          appendRecommendedPlaces(localSpotsList);
+        }
+      });
+  }
+
+  function createLocalSpotObjects(result) {
+    var localSpotsObjects = [];
+    for(var index = 0; index < result.length; index++) {
+      localSpotsObjects.push({
+        name: result[index].placeName,
+        place: "那覇市",
+        img: "./assets/assets/images/spot/0" + (index + 1) + ".jpg"
+      });
+      //getPhotoOfLocation(result[index].lat, result[index].lng);
+    }
+    return localSpotsObjects;
+  }
+
+  function getPhotoOfLocation(lat, lon) {
+    const APIKEY = "AIzaSyB6fdzCM_HlBDacFwkuGoqeWrpfWsjvVn0";
+    var url = "https://maps.googleapis.com/maps/api/place/radarsearch/json?location=" + lat + "," + lon + "&radius=5000&type=museum&key=" + APIKEY;
+    $.ajax({
+        type: "GET",
+        dataType: 'jsonp',
+        url: url,
+        success: function(result) {
+          console.log(JSON.stringify(result));
+        },
+        error: function(result) {
+          console.log(JSON.stringify(result));
+        }
+      });
+  }
+
 
 });
