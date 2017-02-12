e17c417426da3d9be9bc4e4bc0a4109c31efa570
Jacob Biros <jake@Jacobs-MacBook-Pro.local>
2017-02-12 16:12:01 +0900

    updated javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,24 +57,25 @@ $(document).ready(function(){
     var date, time, pay, name, line;
 
     var busInfoArray = [
-    { departureTime: "11:15",
-      arrivalTime: "12:55",
-      sumOfPay: "1,180",
-      objective: "貨物ターミナル前（那覇空港）",
-      recommendationLine: "琉球バス [25]"
-    },
-    { departureTime: "13:25",
-      arrivalTime: "14:12",
-      sumOfPay: "1,180",
-      objective: "貨物ターミナル前（那覇空港）",
-      recommendationLine: "琉球バス [25]"
-    },
-    { departureTime: "14:11",
-      arrivalTime: "15:28",
-      sumOfPay: "1,180",
-      objective: "貨物ターミナル前（那覇空港）",
-      recommendationLine: "琉球バス [25]"
-    }
+      { departureTime: "11:15",
+        arrivalTime: "12:55",
+        sumOfPay: "1,180",
+        objective: "貨物ターミナル前（那覇空港）",
+        recommendationLine: "琉球バス [25]"
+      },
+      { departureTime: "13:25",
+        arrivalTime: "14:12",
+        sumOfPay: "1,180",
+        objective: "貨物ターミナル前（那覇空港）",
+        recommendationLine: "琉球バス [25]"
+      },
+      { departureTime: "14:11",
+        arrivalTime: "15:28",
+        sumOfPay: "1,180",
+        objective: "貨物ターミナル前（那覇空港）",
+        recommendationLine: "琉球バス [25]"
+      }
+    ];
 
     var aquariumInfoArray = [
       { departureTime: "11:15",
@@ -95,9 +96,8 @@ $(document).ready(function(){
         objective: "貨物ターミナル前（那覇空港）",
         recommendationLine: "沖縄バス [120]名護西空港線 名護バスターミナル行"
       }
-    ]
+    ];
 
-  ]
   for(var index=0; index<busInfoArray.length;index++) {
      $(".bus-stop-list").append(`<a class="bus-stop" href="./stopinfo/`+ (index + 1) +`">
          <div class="time-area">
